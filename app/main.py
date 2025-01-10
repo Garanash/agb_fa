1d3355a739aa12f3147a439c6e962ffe699e4e37
a.dolgov <dolgov_am@mail.ru>
2025-01-10 17:50:12 +0300

    mini fixed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
-from importlib import reload
-
-import uvicorn
 from fastapi import FastAPI
 from app.items.router import router as items_router
 from app.users.router import router as users_router
@@ -12,7 +9,3 @@ app.include_router(users_router)
 @app.get("/", tags=['base'])
 async def main_rout():
     return {"status": 200}
-
-
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", port=8000, reload=True)
